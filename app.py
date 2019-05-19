bf6455d9b9de68777c48946a1b4c6b204d895ee4
linnil1 <linnil1.886@gmail.com>
2019-05-19 15:27:39 +0800

    Add history

--- app.py
+++ app.py
@@ -3,16 +3,22 @@ import door
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+with app.app_context():
+    door.db.init_app(app)
+    door.db.create_all()
 
 
 @app.route("/")
 def main():
-    return render_template('index.html')
+    return render_template('index.html', op_str=door.show())
 
 
 @app.route("/open", methods=["POST"])
 def doorOpen():
     door.doorOpen()
+    door.add('who', 'open')
     return redirect('/')
 
 
--- door.py
+++ door.py
@@ -1,19 +1,52 @@
-from gpiozero import Button, LED
+# from gpiozero import Button, LED
 import time
+import datetime
+from flask_sqlalchemy import SQLAlchemy
 
 
-set_relay = LED(4)
+db = SQLAlchemy()
+# on off open
+state = 'off'
+# set_relay = LED(4)
 
 
 def set_on():
+    state = 'on'
     set_relay.on()
 
 
 def set_off():
+    state = 'off'
     set_relay.off()
 
 
 def doorOpen():
-    set_on()
+    # set_on()
     time.sleep(1)
-    set_off()
+    # set_off()
+
+
+class History(db.Model):
+    __tablename__ = 'history'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.String)
+    method = db.Column(db.String)
+    date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+
+    timef = '%m/%d, %H:%M:%S'
+
+    def __repr__(self):
+        return f'{self.user} {self.method} at {self.date.strftime(self.timef)}'
+
+
+def add(user, method):
+    db.session.add(History(user=user, method=method))
+    db.session.commit()
+
+
+def show(limit=5):
+    return db.session.query(History)\
+                     .order_by(History.id.desc())\
+                     .limit(limit)\
+                     .all()
